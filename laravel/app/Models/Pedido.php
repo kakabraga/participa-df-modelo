6764ee1da0e0b15651413f9b7c649d8ae7b49f7c
Cauê Braga <cauebraga.dev@gmail.com>
2026-01-08 11:00:23 -0300

    criação da model e migrations

--- laravel/app/Models/Pedido.php
+++ laravel/app/Models/Pedido.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'hash_texto',
+        'resultado',
+        'origem',
+        'confianca',
+
+    ];
+    public function savePedidoRegex(array $detecoes, string $texto)
+    {
+        return self::create([
+            'hash_texto' => hash('sha256', $texto),
+            'resultado' => 'dados_pessoais_detectados',
+            'origem' => 'regex',
+            'confianca' => 1.0,
+            'tipo_dado' => implode(',', $tiposDetectados = array_column($detecoes, 'tipo'))
+        ]);
+    }
 }
---
+++ laravel/database/migrations/2026_01_08_132503_add_column_pedidos.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pedidos', function (Blueprint $table) {
+            $table->string('tipo_dado')->after('resultado')->nullable(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('pedidos', function (Blueprint $table) {
+            $table->dropColumn('tipo_dado');
+        });
+    }
+};
